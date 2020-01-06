f63d41d7d127dd3410368d803e14dc9978d0e724
Simon Katz <sk@nomistech.com>
2020-01-06 19:01:19 +0000

    Upgrade to Clojure 1.10.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/clojuredatascience/ch4-classification"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [incanter/incanter "1.5.5"]
                  [cc.artifice/clj-ml "0.5.1"]]
   :resource-paths ["resources" "data"]
